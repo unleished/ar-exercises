db01e6b608eb56e7108353caaf7a7394907339e1
Leisha Magsisi <leisha.gallagher@gmail.com>
2018-09-11 16:39:56 +0000

    adding in exercise 1 and 2

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@
 source "https://rubygems.org"
 
 gem 'pry' # for debugging
-gem 'pg'
+gem 'pg', '~> 0.18.4'
 gem 'activerecord'
--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,26 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+
+store = Store.create!(
+  name: 'Burnaby',
+  annual_revenue: 300_000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+store = Store.create!(
+  name: 'Richmond',
+  annual_revenue: 1_260_000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+store = Store.create!(
+  name: 'Gastown',
+  annual_revenue: 190_000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+puts Store.count
